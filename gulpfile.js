c575448e3d93c5da99ec6f2d794771af52f46a16
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2018-12-26 19:39:33 +0200

    Add some plugins but doesnt work task build

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const uglify = require('gulp-uglify');
 // Gulp function for gulp task styles-libs
 function stylesLibs() {
   var cssPaths = [
+    './node_modules/bootstrap/dist/css/bootstrap.css',
     './node_modules/normalize.css/normalize.css',
   ]
   return gulp.src(cssPaths)
@@ -19,6 +20,19 @@ function stylesLibs() {
       level: 2
     }))
     .pipe(gulp.dest('./build/css'))
+}
+
+// Gulp function for gulp task scriptLibs
+function scriptLibs(){
+  const scriptLibs = [
+    './node_modules/bootstrap/dist/js/bootstrap.js'
+  ]
+  return gulp.src(scriptLibs)
+    .pipe(concat('libs.js'))
+    .pipe(uglify({
+        toplevel:true
+    }))
+    .pipe(gulp.dest('./build/js'));
 }
 
 // Gulp function for gulp task Custom styles
@@ -47,7 +61,25 @@ function script(){
     .pipe(gulp.dest('./build/js'));
 }
 
+//Gulp Watch
+function watch(){
+  gulp.watch('./src/scss/**/*.scss', styles);
+  gulp.watch('./src/js/**/*.js', script);
+}
+
+//Gulp Remove all in ./build
+function clean(){
+  return del(['build/*'])
+}
+
 //Gulp Tasks
 gulp.task('styles-libs', stylesLibs);
+gulp.task('script-libs', scriptLibs);
+
 gulp.task('styles', styles);
 gulp.task('script', script);
+gulp.task('watch', watch);
+gulp.task('clean', clean);
+
+gulp.task('libs', gulp.parallel(stylesLibs, scriptLibs));
+gulp.task('build', gulp.series(clean, gulp.parallel(stylesLibs, scriptLibs)));
