f04c8fe4c3b05d9b0d75d6a09c69043bc22ce44b
liquidum <tolikdanylyuk@gmail.com>
2019-01-10 13:51:18 +0200

    First: add fonts some icons and colors

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ function stylesLibs() {
 // Gulp function for gulp task scriptLibs
 function scriptsLibs(){
   const scriptsLibs = [
+    './node_modules/jquery/dist/jquery.js',
     './node_modules/bootstrap/dist/js/bootstrap.js'
   ]
   return gulp.src(scriptsLibs)
