28ccafe17ad91d8d0b23757a0cc42d42234c31b7
liquidum <tolikdanylyuk@gmail.com>
2019-01-10 21:22:39 +0200

    WEbinar info 100%

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,42 @@
-console.log('Hello')
+$(function(){
+  headerHeight();
+  titleAlign()
+  $(window).scroll(function(){
+    
+  });
+
+  $(window).resize(function(){
+    headerHeight();
+  });
+
+});
+
+//webinar height
+function headerHeight(){
+  setTimeout(function(){
+    var height = $('header.header').outerHeight();
+    console.log(height);
+    $('section.webinar-info').css('min-height','calc(100vh - '+ height +'px)');
+  },111);
+}
+
+//webinar title align
+function titleAlign(){
+  setTimeout(function(){
+    var items = $('.webinar-info h2');
+    var max;
+    var min;
+    var height;
+    if($(items[0]).outerHeight() <= $(items[1]).outerHeight()){
+      min = $(items[0]).outerHeight();
+      max = $(items[1]).outerHeight();
+      height = max - min;
+      $(items[0]).css('marginBottom',parseFloat($(items[0]).css('marginBottom'))+ height + 'px');
+    } else {
+      min = $(items[1]).outerHeight();
+      max = $(items[0]).outerHeight();
+      height = max - min;
+      $(items[1]).css('marginBottom',parseFloat($(items[1]).css('marginBottom')) + height + 'px');
+    }
+  },111);
+}
