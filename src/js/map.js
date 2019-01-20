c388e20b01165f43cb948c9fc2866286bd86f00d
liquidum <tolikdanylyuk@gmail.com>
2019-01-20 18:23:20 +0200

    MAde new markers for map show window info, state disabled when choice city, list item text

--- src/js/map.js
+++ src/js/map.js
@@ -57,7 +57,14 @@ $(document).ready(function(){
   $('.seminare__search__filter select').change(function(){   
     filterSort(selectState.val(),selectCity.val(),selectType.val());
   });
-  
+  $('.seminare__search__filter__city select').change(function(){
+    if( !selectState.val() && selectCity.val()){
+      selectState.prop('disabled', true);
+    } else {
+      selectState.prop('disabled', false);
+    }
+    filterSort(selectState.val(),selectCity.val(),selectType.val());
+  });
   $('.seminare__search__filter__state select').change(function(){
     if($(this).val()){
       filterValues.cities = [];
@@ -78,7 +85,15 @@ $(document).ready(function(){
     }
     filterSort(selectState.val(),selectCity.val(),selectType.val());
   });
-
+  $(document).on('click','.marker-info',function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass('active')
+    } else {
+      $('.marker-info').removeClass('active');
+      $(this).addClass('active');
+    }
+    
+  });
   $.when($.ajax({
     type: "GET",
     url:  url,
@@ -111,6 +126,7 @@ $(document).ready(function(){
       selectType.append(option);
     });
     buildSearchList();
+
   });
 });
 
@@ -147,7 +163,18 @@ function buildSearchList(){
   ul.html('');
   searchResult.map(function(item){
     li = document.createElement('li');
-    $(li).html('<h3>'+item.name+'</h3><div><p>' + item.street + ' ' + item.zip + ' ' + item.city_id[1] + '</p><a class="button blue" href="event/' + item.id + '"><span>Platz reservieren</span></a></div><span>Am ' + String(item.date_begin).substr(0, 9) + ' von ' + String(item.date_begin).substr(11,item.date_begin.length-3) + ' bis ' + String(item.date_end).substr(11,item.date_begin.length-3) + ' Uhr</span>');
+    var street = item.street ? item.street : '';
+    var zip = item.zip ? item.zip : '';
+    var city = item.city_id[1] ? item.city_id[1] : '';
+    var date = String(item.date_begin).substr(0, 9).replace('-','.').replace('-','.');
+    date = date.split('.');
+    date = date.reverse();
+    date = date.join('.');
+    var timeStart = String(item.date_begin).replace(':','.').replace(':','.').substr(11,15);
+    var timeEnd = String(item.date_end).replace(':','.').replace(':','.').substr(11,15);
+    timeStart = timeStart.substr(0,5);
+    timeEnd = timeEnd.substr(0,5);
+    $(li).html('<h3>'+item.name+'</h3><div><p>' + street + ' ' + zip + ' ' + city + '</p><a class="button blue" href="event/' + item.id + '"><span>Platz reservieren</span></a></div><span>Am ' + date + ' von ' + timeStart + ' bis ' + timeEnd + ' Uhr</span>');
     $(li).appendTo(ul);
   });
   
@@ -169,20 +196,31 @@ function initMap() {
   // addMarker(haightAshbury);
 }
 
-function addMarker(location) {
+function addMarker(location,contentString) {
   var marker = new google.maps.Marker({
     position: location,
     map: map
   });
+  marker.setVisible(false);
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infowindow.open(map, marker);
   markers.push(marker);
 }
 
 function createMarkersGroup(items){
   items.map(function(item){
+    console.log(item);
+    var street = item.street ? item.street + ' ,' : '';
+    var city = item.city_id[1] ? item.city_id[1] + ' ,' : '';
+    var country =  item.country_id[1] ? item.country_id[1] : '';
+    var zip = item.zip ? item.zip : '';
+    var contentString = '<div class="marker-info"><div class="marker-info-title"><span></span><h2>'+item.city_id[1]+'</h2></div><div class="marker-info-content"><a href="tel:123" class="phone">123123123123</a><p class="address"><span>'+ street +'</span><span>'+ zip +' '+ city +'</span> <span>'+ country +'</span></p></div> </div>';
     addMarker({
       lat: item.latitude, 
       lng: item.longitude
-    });
+    },contentString);
   }); 
 }
 
@@ -204,6 +242,66 @@ function deleteMarkers() {
   clearMarkers();
   markers = [];
 }
+
+
+//Custom Marker HTML
+
+// function CustomMarker(latlng, map, args) {
+// 	this.latlng = latlng;	
+// 	this.args = args;	
+// 	this.setMap(map);	
+// }
+
+// CustomMarker.prototype = new google.maps.OverlayView();
+
+// CustomMarker.prototype.draw = function() {
+	
+// 	var self = this;
+	
+// 	var div = this.div;
+	
+// 	if (!div) {
+	
+// 		div = this.div = document.createElement('div');
+		
+// 		div.className = 'marker';
+		
+// 		div.style.position = 'absolute';
+// 		div.style.cursor = 'pointer';
+// 		div.style.width = '20px';
+// 		div.style.height = '20px';
+// 		div.style.background = 'blue';
+		
+// 		if (typeof(self.args.marker_id) !== 'undefined') {
+// 			div.dataset.marker_id = self.args.marker_id;
+// 		}
+		
+// 		google.maps.event.addDomListener(div, "click", function(event) {			
+// 			google.maps.event.trigger(self, "click");
+// 		});
+		
+// 		var panes = this.getPanes();
+// 		panes.overlayImage.appendChild(div);
+// 	}
+	
+// 	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+	
+// 	if (point) {
+// 		div.style.left = point.x + 'px';
+// 		div.style.top = point.y + 'px';
+// 	}
+// };
+
+// CustomMarker.prototype.remove = function() {
+// 	if (this.div) {
+// 		this.div.parentNode.removeChild(this.div);
+// 		this.div = null;
+// 	}	
+// };
+
+// CustomMarker.prototype.getPosition = function() {
+// 	return this.latlng;	
+// };
 
 
 
