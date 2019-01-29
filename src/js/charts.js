31e0fb807cda5dced1be3190529be0fce0647d88
liquidum <tolikdanylyuk@gmail.com>
2019-01-30 00:55:14 +0200

    Bad repair charts

--- src/js/charts.js
+++ src/js/charts.js
@@ -1,19 +1,25 @@
 var chartsData;
 var urlCharts = 'http://localhost:3000/json/charts-data.json';
+var ctxOne;
+var ctxTwo;
+var myChartOne;
+var myChartTwo;
+var dataOne;
+var dataTwo;
+var options;
 
 $(document).ready(function () {
     // var myChartOne;
     // var myChartTwo;
-    $.when(getButtonData(urlCharts)).done(function () {
-       
-    });
-    var ctxOne = document.getElementById("myChartOne").getContext('2d');
-    var ctxTwo = document.getElementById("myChartTwo").getContext('2d');
+    
+   
+    ctxOne = document.getElementById("myChartOne").getContext('2d');
+    ctxTwo = document.getElementById("myChartTwo").getContext('2d');
     Chart.plugins.unregister(ChartDataLabels);
     Chart.defaults.global.defaultFontColor = '#292929';
     Chart.defaults.global.defaultFontSize = 14;
     Chart.defaults.global.defaultFontFamily = 'Poppins-SemiBold';
-    var options = {
+    options = {
         display: true,
         layout: {
             padding: {
@@ -41,47 +47,6 @@ $(document).ready(function () {
             }]
         }
     };
-    var dataOne = {
-        labels: chartsData.label,
-        datasets: [{
-            data: chartsData.chart[0].data1,
-            backgroundColor: '#44acd7',
-            borderColor: '#44acd7',
-            borderWidth: 1
-        }]
-    };
-    var dataTwo = {
-        labels:  chartsData.label,
-        datasets: [{
-            data:  chartsData.chart[0].data2,
-            backgroundColor: '#44acd7',
-            borderColor: '#44acd7',
-            borderWidth: 1
-        }]
-    };
-    var myChartOne = new Chart(ctxOne, {
-        type: 'bar',
-        data: dataOne,
-        plugins: [ChartDataLabels],
-        options: options
-    });   
-    var myChartTwo = new Chart(ctxTwo, {
-        type: 'bar',
-        data: dataTwo,
-        plugins: [ChartDataLabels],
-        options: options
-    });
-    var chartsButtonsContainer = $('.charts__buttons');
-    for(var i = 0; i < chartsData.percents.length; i++){
-        var button = document.createElement('button');
-        button.value = chartsData.percents[i];
-        button.classList.add('button');
-        button.classList.add('red');
-        $(button).html('<span>' + chartsData.percents[i] + '%</span>');
-        $(button).appendTo(chartsButtonsContainer);
-    }
-    
-    $('.charts__buttons')
     $(window).resize(function () {
         if (window.outerWidth <= 991) {
             Chart.defaults.global.defaultFontSize = 12;
@@ -98,13 +63,27 @@ $(document).ready(function () {
             options.layout.padding.left = 20;
             new Chart(ctxOne, {
                 type: 'bar',
-                data: dataOne,
+                data: dataTwo,
                 plugins: [ChartDataLabels],
                 options: options
             });
 
         }
     });
+    $.when(getButtonData(urlCharts)).done(initsCharts(chartsData.label, chartsData.chart[0].data1, chartsData.chart[0].data2 ));
+    var chartsButtonsContainer = $('.charts__buttons');
+    for(var i = 0; i < chartsData.percents.length; i++){
+        var button = document.createElement('button');
+        button.value = chartsData.percents[i];
+        button.classList.add('button');
+        button.classList.add('red');
+        $(button).html('<span>' + chartsData.percents[i] + '%</span>');
+        $(button).appendTo(chartsButtonsContainer);
+        button.addEventListener('click', function(){
+            getButtonData(urlCharts);
+            initsCharts(chartsData.label, chartsData.chart[1].data1, chartsData.chart[1].data2 )
+        });
+    }
 });
 
 function getButtonData(url){
@@ -120,5 +99,38 @@ function getButtonData(url){
         error: function error(_error) {
             console.log('error: ', _error);
         }
-    })
+    });
+};
+
+function initsCharts(labels, data1, data2 ){
+    dataOne = {
+        labels: labels || chartsData.label,
+        datasets: [{
+            data:data1 || chartsData.chart[0].data1,
+            backgroundColor: '#44acd7',
+            borderColor: '#44acd7',
+            borderWidth: 1
+        }]
+    };
+    dataTwo = {
+        labels: labels ||  chartsData.label,
+        datasets: [{
+            data: data2 || chartsData.chart[0].data2,
+            backgroundColor: '#44acd7',
+            borderColor: '#44acd7',
+            borderWidth: 1
+        }]
+    };
+    myChartOne = new Chart(ctxOne, {
+        type: 'bar',
+        data: dataOne,
+        plugins: [ChartDataLabels],
+        options: options
+    });   
+    myChartTwo = new Chart(ctxTwo, {
+        type: 'bar',
+        data: dataTwo,
+        plugins: [ChartDataLabels],
+        options: options
+    });
 }
